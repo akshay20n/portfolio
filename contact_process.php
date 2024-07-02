14d6f4dfe47fe337d5c5b6a67c0e3259c80af5de
Akshay Navale <169084830+akshay20n@users.noreply.github.com>
2024-07-03 00:01:07 +0530

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "akshaynavale5915@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
